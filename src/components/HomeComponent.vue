f8cb06e8b1422864bcab3cd8b12bb4e81b146272
ninaperssoon <ninap21@hotmail.com>
2022-11-24 12:50:35 +0100

    HomeComponent

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="homeButton">
+    <router-link to="/"><img id="homePic" src="https://cdn-icons-png.flaticon.com/512/15/15766.png"/></router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeComponent',
+  props: {
+      homeButton: Object
+    },
+  
+}
+</script>
+<style scoped>
+#homeButton {
+  margin-right: 100%;
+  padding: 0.4em;
+}
+#homePic {
+  height: 3em;
+  width: 3em;
+  cursor: pointer;
+}
+
+ 
+</style>
