6a001d7edb5f6dba1f691db2236c89137c038fcc
Iason-kaxiras <ikaxiras@gmail.com>
2022-11-25 14:43:37 +0100

    new component

---
+++ src/components/EditableQuestion.vue
@@ -0,0 +1,213 @@
+
+
+<template>
+    <div>
+<div id="question" >
+    <div id="inputQuestion">
+       {{question.number}}
+      <input type="text" v-model="name" class="input" placeholder="Write your question here...">
+      <br>
+    </div>
+    
+
+    <div id="inputAnswer" v-for="(_,i) in ansBox" v-bind:key="'ansBox' + i">
+      <div v-for="(_,i) in answers" v-bind:key="'answer' + i">
+
+        <input type="text" v-model="answers[i]" class="input" id="option" placeholder="Write your answers here...">
+        <button v-on:click="deleteAnswers" class="deleteB" id="deleteAnswerButton">
+            -
+        </button>
+
+        <input type="checkbox" id="checked" value:answer v-model="selectedAnswers">
+        <br>
+      </div>
+
+      <button v-on:click="addAnswer" id="addAnswerButton" value:answer>
+          +
+      </button>
+
+    </div>
+
+      <!--<div v-for="(_, i) in ansBox" v-bind:key="'ansBox'+i">
+        <div id="ansBox">
+          <input type="text" v-model="ansBox[i]" class="input" id="option" placeholder="Write your answes here...">
+        </div>
+      
+        <div class="answer" v-for="(_, i) in answers" v-bind:key="'answer'+i">
+          
+            <input type="checkbox" id="checked" value:answer v-model="selectedAnswers">
+          <button v-on:click="deleteAnswers" class="deleteB" id="deleteAnswerButton">
+            -
+          </button>
+        </div>
+        <button v-on:click="addAnswer" id="addAnswerButton">
+          +
+        </button>
+
+      </div>-->
+
+    <div>
+    
+    <!--<input type="number" v-model="questionNumber">-->
+    <button v-on:click="deleteQuestion" class="deleteB" id="deleteQuestions">
+      Delete question
+    </button >
+    <button v-on:click="sendQuestion">
+    Send question
+    </button>
+    <!--{{data}}
+    <router-link v-bind:to="'/result/'+pollId">Check result</router-link>-->
+  </div>
+
+
+</div>
+
+</div>
+
+</template>
+
+<script>
+
+export default{
+name : 'editableQuestion',
+props: { 
+    question: Object
+
+},
+
+    data: function(){
+        return{
+            selectedAnswers : [],
+            answeralt: [],
+            name: "",
+            ansBox: [""],
+            answers: ["",""]
+
+
+
+        }
+
+    },
+
+    methods:{
+  
+     addAnswer: function () {
+      this.answers.push("");
+    },
+      deleteAnswers: function() {
+      this.answers.pop("");
+      
+    },
+    deleteQuestion: function () {
+      console.log()
+      this.questions.pop("") //pop = delete/pull
+    },
+    sendQuestion: function(){
+      this.$emit('myquestion',{name: this.name, answer: this.answers})
+    }
+    
+
+    }
+}
+
+</script>
+
+
+
+
+
+<style>
+
+#deleteAnswerButton {
+  height: 2em;
+  width: 2em;
+  grid-column: 2;
+  grid-row: 2;
+  
+}
+
+#checked {
+  height: 2em;
+  width: 2em;
+  /*grid-column: 2;
+  grid-row: 1;*/
+  
+
+}
+
+#question {
+  margin: 2em 8em 2em 8em;
+  display: grid;
+  /*grid-template-columns: 1fr ;*/
+  /*row-gap: 50px;
+  column-gap: 30px;*/
+  background-color:lightblue;
+  border-radius: 2em;
+  padding: 1em;
+}
+
+.answer {
+  display: grid;
+  
+}
+#inputAnswer {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr ;
+  column-gap: 0em;
+  row-gap: 2em;
+  padding: 1em;
+  grid-auto-rows: 4em;
+  column-gap: 1em;
+}
+
+
+
+.deleteB:hover {
+  background-color: salmon;
+  cursor:pointer;
+}
+
+#deleteQuestions {
+  width: 5em;
+  height: 5em;
+  margin-left: 80em;
+  border-radius: 2em;
+  border-radius: 1em;
+}
+
+
+#option {
+  border-radius: 1em;
+ 
+  height: auto;
+  padding: 1em 1em;
+  margin: 0 2em 0 0;
+  /*grid-column: 1 / span 2;
+  grid-row: 1 / span 2;*/
+
+  box-sizing:border-box;
+} 
+#inputQuestion {
+  margin: 2em 2em 2em 2em;
+  padding-right: 2em;
+}
+
+#addAnswerButton {
+  width: 3.5em;
+  height: 3.5em;
+  border-radius: 50%;
+  margin: 18px 0;
+}
+.input {
+  border-radius: 1em;
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+}
+
+
+
+
+
+</style>
--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,4 +1,6 @@
 <template>
+  <div>
+
   <div class="pollLink">
       Poll link: 
       <input type="text" v-model="pollId">
@@ -6,91 +8,55 @@
        Create poll
       </button>
     </div>
-  <div v-for="(_,i) in questions" v-bind:key="'question' + i">
+
+
+ <div>
+   <Question v-for="question in questions" 
+  v-bind:question="question"
+  v-bind:key="question.name"
+  v-on:myquestion="saveQuestion($event)"/>
     
-    <div id="question" >
-      <div id="inputQuestion">
-        {{uiLabels.question}}: {{i}}
-        <input type="text" v-model="question[i]" class="input" placeholder="Write your question here...">
-        <br>
-      </div>
-      
-
-      <div id="inputAnswer" v-for="(_,i) in ansBox" v-bind:key="'ansBox' + i">
-        <div v-for="(_,i) in answers" v-bind:key="'answer' + i">
-
-          <input type="text" v-model="question[i]" class="input" id="option" placeholder="Write your answers here...">
-          <button v-on:click="deleteAnswers" class="deleteB" id="deleteAnswerButton">
-              -
-          </button>
-
-          <input type="checkbox" id="checked" value:answer v-model="selectedAnswers">
-          <br>
-        </div>
-
-        <button v-on:click="addAnswer" id="addAnswerButton" value:answer>
-            +
-        </button>
-
-      </div>
-
-        <!--<div v-for="(_, i) in ansBox" v-bind:key="'ansBox'+i">
-          <div id="ansBox">
-            <input type="text" v-model="ansBox[i]" class="input" id="option" placeholder="Write your answes here...">
-          </div>
-        
-          <div class="answer" v-for="(_, i) in answers" v-bind:key="'answer'+i">
-            
-              <input type="checkbox" id="checked" value:answer v-model="selectedAnswers">
-            <button v-on:click="deleteAnswers" class="deleteB" id="deleteAnswerButton">
-              -
-            </button>
-          </div>
-          <button v-on:click="addAnswer" id="addAnswerButton">
-            +
-          </button>
-
-        </div>-->
-
-      <div>
-      
-      <!--<input type="number" v-model="questionNumber">-->
-      <button v-on:click="deleteQuestion" class="deleteB" id="deleteQuestions">
-        Delete question
-      </button>
-      <!--{{data}}
-      <router-link v-bind:to="'/result/'+pollId">Check result</router-link>-->
-    </div>
-
-
-  </div>
-  
-  </div>
-  <button v-on:click="addQuestion">
+  <button v-on:click="newQuestion">
         Add question
   </button>
+</div>
+
+
+</div>
 </template>
 
 <script>
 import io from 'socket.io-client';
+import Question from '/Users/jason/Documents/github/polly/src/components/EditableQuestion.vue';
+
 const socket = io();
+
+//function singleQuestion (nm, alt, txt){
+ //this.text = txt;
+ //this.alternatives = alt;
+// this.number = nm;
+//}
+
 
 
 export default {
   name: 'CreateView',
+  components:{
+    Question,
+  },
   data: function () {
     return {
       lang: "",
       pollId: "",
-      question: "",
-      answers: ["", ""],
-      questions:[""],
-      questionNumber: 0,
-      data: {},
-      uiLabels: {},
-      selectedAnswers: [],
-      ansBox: [""],
-      text: "",
+      questions: [],
+      uiLabels:{},
+      answers: {},
+      question: ""
+    
+      
+      
+      
+      
     }
   },
   created: function () {
@@ -110,22 +76,26 @@ export default {
       socket.emit("createPoll", {pollId: this.pollId, lang: this.lang })
     },
     addQuestion: function () {
-      //socket.emit("addQuestion", {pollId: this.pollId, q: this.question, a: this.answers } )
-      this.questions.push("")
-    },
-    addAnswer: function () {
-      this.answers.push("");
-    },
-    deleteAnswers: function() {
-      this.answers.pop("");
+       socket.emit("addQuestion", {pollId: this.pollId, q: this.question, a: this.answers } )
       
     },
+    newQuestion: function(){
+      this.questions.push("")
+    },
+    
+    
+   
     runQuestion: function () {
       socket.emit("runQuestion", {pollId: this.pollId, questionNumber: this.questionNumber})
     },
-    deleteQuestion: function () {
-      this.questions.pop("") //pop = delete/pull
+    saveQuestion: function (event) {
+      this.answers = event.answer;
+      this.question = event.name;
+      this.addQuestion();
+      console.log(this.question) 
+      console.log(this.answers);
     }
+   
   }
 }
 </script>
@@ -155,99 +125,21 @@ button:hover {
 
 }
 
-#question {
-  margin: 2em 8em 2em 8em;
-  display: grid;
-  /*grid-template-columns: 1fr ;*/
-  /*row-gap: 50px;
-  column-gap: 30px;*/
-  background-color:lightblue;
-  border-radius: 2em;
-  padding: 1em;
-}
-
-.answer {
-  display: grid;
-  
-}
-#inputAnswer {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  column-gap: 0em;
-  row-gap: 2em;
-  padding: 1em;
-  grid-auto-rows: 4em;
-}
-
-
-
-.deleteB:hover {
-  background-color: salmon;
-  cursor:pointer;
-}
-
-#deleteQuestions {
-  width: 5em;
-  height: 5em;
-  margin-left: 80em;
-  border-radius: 2em;
-  border-radius: 1em;
-}
-
 #pollButton {
   margin-top: 1em;
   border-radius: 1em;
 }
 
-.input {
-  border-radius: 1em;
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
-}
 
 
 
-#inputQuestion {
-  margin: 2em 2em 2em 2em;
-  padding-right: 2em;
-}
 
-#addAnswerButton {
-  width: 3.5em;
-  height: 3.5em;
-  border-radius: 50%;
-  margin: 18px 0;
-}
 
-#option {
-  border-radius: 1em;
-  width: 20em;
-  height: auto;
-  padding: 1em 1em;
-  margin: 0 2em 0 0;
-  /*grid-column: 1 / span 2;
-  grid-row: 1 / span 2;*/
 
-  box-sizing:border-box;
-} 
 
-#checked {
-  height: 2em;
-  width: 2em;
-  /*grid-column: 2;
-  grid-row: 1;*/
-  
 
-}
 
-#deleteAnswerButton {
-  height: 2em;
-  width: 2em;
-  grid-column: 2;
-  grid-row: 2;
-  
-}
+
+
 
 </style>
