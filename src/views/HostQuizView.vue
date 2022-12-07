3ef74c351797cb42c62ab96c5fcbd58ee8fbcfdc
ninaperssoon <ninap21@hotmail.com>
2022-12-07 09:19:34 +0100

    hostquizview

---
+++ src/views/HostQuizView.vue
@@ -0,0 +1,120 @@
+<template>
+  <body>
+    <homeButton></homeButton>
+    <section>
+      <h1>{{uiLabels.hostQuiz}}</h1>
+      <div id="host">
+        <p> {{uiLabels.writeQuizId}}: 
+          <br>
+          <input type="text" v-model="pollId">  </p>
+        
+        
+              
+      </div>
+
+     <!-- Kom ihåg att ändra länken nedan!!!! -->
+      <router-link id="startQuiz" v-bind:to="('/host/'+id+lang)">{{uiLabels.startQuiz}}</router-link> 
+   
+      <div></div>
+
+    </section>
+  
+</body>
+
+</template>
+
+<script>
+import homeButton from '@/components/HomeComponent.vue';
+import io from 'socket.io-client';
+const socket = io();
+
+export default {
+  name: 'HostQuizView',
+  components: {
+    homeButton,
+  },
+  data: function () {
+    return {
+      uiLabels: {},
+      id: "",
+      lang: "en",
+    }
+  },
+  created: function () {
+    this.lang = this.$route.params.lang;
+    socket.emit("pageLoaded", this.lang);
+    socket.on("init", (labels) => {
+      this.uiLabels = labels
+    })
+  },
+  methods: {
+    switchLanguage: function() {
+      if (this.lang === "en")
+        this.lang = "sv"
+      else
+        this.lang = "en"
+      socket.emit("switchLanguage", this.lang)
+    },
+
+  }
+}
+</script>
+<style scoped>
+  body {
+    margin: 0;
+    background-color: #BCD8C1;
+    
+  }
+
+  div {
+    height: 4.78em;
+    line-height: 3em;
+  }
+
+  #host {
+    font-size:1.5em;
+
+    margin: auto;
+    margin-top: 2em;
+    height: 20em;
+    width: 30em;
+    background-color: #007991;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 2em;
+    border-style: outset;
+    border-color: #0079918f;
+
+  }
+
+  input {
+
+    font-size: 1em;
+    border-radius: 1em;
+    border-style: outset;
+  }
+
+  #startQuiz {
+    font-size:1.5em;
+    background-color: rgb(235, 209, 106);
+    border-color: rgba(235, 209, 106, 0.689);
+    text-decoration: none;
+    height: 2.5em;
+    width: 8em;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    margin-right: 5em;
+    margin-bottom: 5em;
+    border-radius: 2em;
+    border-style: outset;
+    padding-top: 1.5em;
+  }
+
+  #startQuiz:hover {
+    box-shadow: 0 5px 15px #0079918f;
+
+  }
+ 
+</style>
