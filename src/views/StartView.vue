8648d0849f990f856ded8f29e87de003c6cde2da
Lovisa Nilsson <lovisanilsson@MacBook-Air-som-tillhor-Lovisa-7.local>
2022-12-12 12:38:44 +0100

    Flip or Flop

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -5,7 +5,7 @@
     <div v-bind:class="['hamburger', {'close': !hideNav}]" 
          v-on:click="toggleNav">
     </div>
-    <div class="logo"> punishment play: the flip. Also known as "Flip or Flop" </div>
+    <div class="logo"> Flip or Flop </div>
   </header>
   <ResponsiveNav v-bind:hideNav="hideNav">
     <img id="flag" v-bind:src=uiLabels.languageFlag v-on:click="switchLanguage">
